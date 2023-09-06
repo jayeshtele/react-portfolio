f51330d5632eec3958bc2ccd77265ed9a5ca81a5
Jayesh Tele <110726388+jayeshtele@users.noreply.github.com>
2023-09-06 22:58:26 +0530

    Update

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
 
 export default function SideNav() {
   return (
-    <div>
-     
+    <div className="w-[7%] ">
+      <figure className="cursor-pointer">
+        <img className="w-[60%] rounded-full" src="/assets/Profile.png" alt="Profile" />
+      </figure>
+      <div className="icons"></div>
     </div>
-  )
+  );
 }
