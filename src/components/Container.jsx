6cdd60c23e1ed0c1be841907d92662c3a8e6970d
Jayesh Tele <110726388+jayeshtele@users.noreply.github.com>
2023-09-07 15:20:36 +0530

    Update Container.jsx

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,5 +1,10 @@
 import React from "react";
+import HeroSection from "./HeroSection";
 
 export default function Container() {
-  return <div>Container</div>;
+  return (
+    <div className="h-screen w-screen">
+      <HeroSection />
+    </div>
+  );
 }
