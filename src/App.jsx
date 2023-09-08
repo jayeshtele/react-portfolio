2bcb110d30abbbe21e9496529c7c4012a7611826
Jayesh Tele <110726388+jayeshtele@users.noreply.github.com>
2023-09-08 11:58:31 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 import SideNav from "./components/SideNav";
+import HeroSection from "./components/HeroSection";
+import AboutMe from "./components/AboutMe";
 import Container from "./components/Container";
 
 export default function App() {
   return (
-    <div className="flex">
+    <div className="flex ">
       <SideNav />
-      <Container />
+      <Routes>
+        <Route index element={<Container />} />
+      </Routes>
     </div>
   );
 }
