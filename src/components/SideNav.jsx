ba631ff7ae9de0485892a43c27abcb655de971f7
Jayesh Tele <110726388+jayeshtele@users.noreply.github.com>
2023-09-08 11:58:37 +0530

    Update SideNav.jsx

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -6,34 +6,50 @@ import CodeIcon from "@mui/icons-material/Code";
 import DesignServicesIcon from "@mui/icons-material/DesignServices";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import { NavLink } from "react-router-dom";
 
 export default function SideNav() {
   return (
-    <div className="sidenav relative flex w-[7%] flex-col items-center justify-around">
+    <div className="sidenav relative flex h-screen w-[7%] flex-col items-center justify-around">
       <figure className="z-10 w-[60%] cursor-pointer">
         <img className="rounded-full" src="/assets/Profile.png" alt="Profile" />
       </figure>
       <div className="sidenav-icons z-10 w-full cursor-pointer">
-        <div className="sidenav-icons-hover relative  flex items-center justify-center">
+        <NavLink
+          to="/"
+          className="sidenav-icons-hover relative  flex items-center justify-center"
+        >
           <HomeIcon />
           <span className="absolute left-6 inline-block">Home</span>
-        </div>
-        <div className="sidenav-icons-hover relative  my-10 flex items-center justify-center">
+        </NavLink>
+        <NavLink
+          to="/about"
+          className="sidenav-icons-hover relative  my-10 flex items-center justify-center"
+        >
           <InfoIcon className="about" />
           <span className="absolute left-6 inline-block">About</span>
-        </div>
-        <div className="sidenav-icons-hover relative flex items-center justify-center">
+        </NavLink>
+        <NavLink
+          to="/certificate"
+          className="sidenav-icons-hover relative flex items-center justify-center"
+        >
           <EmojiEventsIcon className="certificate" />
           <span className="absolute left-6 inline-block">Certificate</span>
-        </div>
-        <div className="sidenav-icons-hover relative my-10 flex items-center justify-center">
+        </NavLink>
+        <NavLink
+          to="/projects"
+          className="sidenav-icons-hover relative my-10 flex items-center justify-center"
+        >
           <CodeIcon className="code" />
           <span className="absolute left-6 inline-block">Projects</span>
-        </div>
-        <div className="sidenav-icons-hover relative flex items-center justify-center">
+        </NavLink>
+        <NavLink
+          to="/services"
+          className="sidenav-icons-hover relative flex items-center justify-center"
+        >
           <DesignServicesIcon className="services" />
           <span className="absolute left-6 inline-block">Services</span>
-        </div>
+        </NavLink>
       </div>
 
       <div className="sidenav-icons z-10 w-full cursor-pointer">
