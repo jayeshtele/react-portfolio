3079025721a90cb80c313818765f0e19c6230e78
Jayesh Tele <110726388+jayeshtele@users.noreply.github.com>
2023-09-08 13:34:12 +0530

    Update Certificate.jsx

--- src/components/Certificate.jsx
+++ src/components/Certificate.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
-export default function Certificate({targetSectionRef}) {
-  return <div id="certificate" ref={targetSectionRef} className="h-full w-full">Certificate</div>;
+export default function Certificate() {
+  return <div className="h-screen w-full">Certificate</div>;
 }
