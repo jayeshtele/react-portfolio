7186848159bb7210731d6a3547a88bee793b8962
Jayesh Tele <110726388+jayeshtele@users.noreply.github.com>
2023-09-08 13:34:10 +0530

    Update AboutMe.jsx

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,9 +1,5 @@
 import React from "react";
 
-export default function AboutMe({ targetSectionRef }) {
-  return (
-    <div id="about" ref={targetSectionRef} className="h-full w-full">
-      About Me
-    </div>
-  );
+export default function AboutMe() {
+  return <div className="h-screen w-full">About Me</div>;
 }
