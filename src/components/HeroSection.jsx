6e6f250ac21126a3ac997c94aa9eeea07c66a9e2
Jayesh Tele <110726388+jayeshtele@users.noreply.github.com>
2023-09-08 19:59:10 +0530

    Update HeroSection.jsx

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,21 +1,45 @@
 import React from "react";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import { NavLink } from "react-router-dom";
 
 export default function HeroSection() {
   return (
-    <div className="hero-section flex flex-col justify-center px-5">
-      <h2 className="text-5xl">Hi! I'm Jayesh</h2>
-      <h1 className="my-5 text-8xl font-semibold">
+    <div className="hero-section relative flex flex-col justify-center px-5">
+      <div className="md:hidden">
+        <figure className="absolute top-3 z-10 w-[10%] cursor-pointer">
+          <img
+            className="rounded-full"
+            src="/assets/Profile.png"
+            alt="Profile"
+          />
+        </figure>
+        <div className="hero-logos absolute right-4 top-3 space-x-3">
+          <NavLink to="https://github.com/jayeshtele" target="_blank">
+            <GitHubIcon />
+          </NavLink>
+          <NavLink
+            to="https://www.linkedin.com/in/jayesh-tele-9150711b3?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BlDa13SKwRLORK1NOJ0eUNg%3D%3D"
+            target="_blank"
+          >
+            <LinkedInIcon />
+          </NavLink>
+        </div>
+      </div>
+
+      <h2 className="text-2xl md:text-3xl xl:text-5xl">Hi! I'm Jayesh</h2>
+      <h1 className="text-4xl font-semibold my-5 xl:text-8xl md:text-5xl">
         Front-End Web Developer
         <span className="block text-[#f52225]">Based In India.</span>
       </h1>
-      <p className="text-xl">
+      <p className="text-xs md:text-sm xl:text-xl">
         I'm developer focused on crafting user friendly experiences, I am
         passionate about building excellent
-        <span className="block">
+        <span className="md:block">
           Web Designs that improves the lives of those around me.
         </span>
       </p>
-      <button className="mt-10 self-start rounded-xl bg-[rgba(119,119,119,0.2)] px-[8rem] py-3 text-left text-xl font-semibold hover:bg-[rgba(119,119,119,0.5)] duration-500 ease-out">
+      <button className="mt-5 self-start rounded-xl bg-[rgba(119,119,119,0.2)] px-[4rem] py-3 text-left font-semibold duration-500 ease-out hover:bg-[rgba(119,119,119,0.5)] xl:mt-10 xl:px-[8rem] xl:text-xl">
         My Work
       </button>
     </div>
