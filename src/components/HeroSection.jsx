3abd48db851a338f08871d9775e1fcb8b2699a38
Jayesh Tele <110726388+jayeshtele@users.noreply.github.com>
2023-09-08 11:00:35 +0530

    Update HeroSection.jsx

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function HeroSection() {
   return (
-    <div className="hero-section flex flex-col justify-center pl-5">
+    <div className="hero-section flex flex-col justify-center">
       <h2 className="text-5xl">Hi! I'm Jayesh</h2>
       <h1 className="my-5 text-8xl font-semibold">
         Front-End Web Developer
@@ -10,12 +10,12 @@ export default function HeroSection() {
       </h1>
       <p className="text-xl">
         I'm developer focused on crafting user friendly experiences, I am
-        passionate about building excellent --container-section-color:
+        passionate about building excellent
         <span className="block">
           Web Designs that improves the lives of those around me.
         </span>
       </p>
-      <button className="mt-10 self-start rounded-xl bg-[rgba(119,119,119,0.2)] px-[8rem] py-3 text-left text-xl font-semibold hover:bg-[rgba(119,119,119,0.5)] duration-500 ease-out">
+      <button className="mt-10 self-start rounded-xl bg-[rgba(119,119,119,0.2)] px-[8rem] py-3 text-left text-xl font-semibold duration-500 ease-out hover:bg-[rgba(119,119,119,0.5)]">
         My Work
       </button>
     </div>
