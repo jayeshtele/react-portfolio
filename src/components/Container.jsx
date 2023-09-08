d3d88abc13fb7e97c00796016150c51e412b2fa7
Jayesh Tele <110726388+jayeshtele@users.noreply.github.com>
2023-09-08 11:58:35 +0530

    Update Container.jsx

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import HeroSection from "./HeroSection";
+import AboutMe from "./AboutMe";
 
 export default function Container() {
   return (
-    <div className="h-full w-full">
+    <div className="h-screen w-screen overflow-y-scroll">
       <HeroSection />
+      <AboutMe />
     </div>
   );
 }
