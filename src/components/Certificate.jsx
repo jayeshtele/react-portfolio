58c1301bed005e7f202d281992dbfe75e62562d4
Jayesh Tele <110726388+jayeshtele@users.noreply.github.com>
2023-09-09 20:27:56 +0530

    Update Certificate.jsx

--- src/components/Certificate.jsx
+++ src/components/Certificate.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Certificate() {
   return (
-    <div className="mb-10">
+    <div id="certificate" className="mb-10">
       <h3 className="pb-5 text-2xl font-bold tracking-widest">Certificates</h3>
       <figure className="grid md:grid-cols-2 gap-5">
         <img src="/assets/Certificate.png" alt="" />
