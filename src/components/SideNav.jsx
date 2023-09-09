61a38574bcd4f3ca3418452ba8453c5f2c8bdbaf
Jayesh Tele <110726388+jayeshtele@users.noreply.github.com>
2023-09-09 20:28:04 +0530

    Update SideNav.jsx

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -7,6 +7,7 @@ import DesignServicesIcon from "@mui/icons-material/DesignServices";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import { NavLink } from "react-router-dom";
+import { HashLink } from "react-router-hash-link";
 
 export default function SideNav() {
   return (
@@ -20,26 +21,38 @@ export default function SideNav() {
           />
         </figure>
         <div className="sidenav-icons z-10 w-full cursor-pointer">
-          <div className="sidenav-icons-hover relative  flex items-center justify-center">
+          <HashLink
+            to="#hero"
+            smooth
+            className="sidenav-icons-hover relative  flex items-center justify-center"
+          >
             <HomeIcon />
             <span className="absolute left-6 inline-block">Home</span>
-          </div>
-          <div className="sidenav-icons-hover relative  my-10 flex items-center justify-center">
+          </HashLink>
+          <HashLink
+            to="#about"
+            smooth
+            className="sidenav-icons-hover relative  my-10 flex items-center justify-center"
+          >
             <InfoIcon className="about" />
             <span className="absolute left-6 inline-block">About</span>
-          </div>
-          <div className="sidenav-icons-hover relative flex items-center justify-center">
+          </HashLink>
+          <HashLink
+            to="#certificate"
+            smooth
+            className="sidenav-icons-hover relative flex items-center justify-center"
+          >
             <EmojiEventsIcon className="certificate" />
             <span className="absolute left-6 inline-block">Certificate</span>
-          </div>
-          <div className="sidenav-icons-hover relative my-10 flex items-center justify-center">
+          </HashLink>
+          <HashLink
+            to="#projects"
+            smooth
+            className="sidenav-icons-hover relative my-10 flex items-center justify-center"
+          >
             <CodeIcon className="code" />
             <span className="absolute left-6 inline-block">Projects</span>
-          </div>
-          <div className="sidenav-icons-hover relative flex items-center justify-center">
-            <DesignServicesIcon className="services" />
-            <span className="absolute left-6 inline-block">Services</span>
-          </div>
+          </HashLink>
         </div>
 
         <div className="sidenav-icons z-10 w-full cursor-pointer">
@@ -64,11 +77,18 @@ export default function SideNav() {
 
       <div className="mob-nav fixed bottom-0 z-10 w-full bg-[#0f1014] py-5 text-[#777] md:hidden">
         <div className="flex justify-evenly">
-          <HomeIcon />
-          <InfoIcon />
-          <EmojiEventsIcon />
-          <CodeIcon />
-          <DesignServicesIcon />
+          <HashLink smooth to="#hero">
+            <HomeIcon />
+          </HashLink>
+          <HashLink smooth to="#about">
+            <InfoIcon />
+          </HashLink>
+          <HashLink smooth to="#certificate">
+            <EmojiEventsIcon />
+          </HashLink>
+          <HashLink smooth to="#projects">
+            <CodeIcon />
+          </HashLink>
         </div>
       </div>
     </>
