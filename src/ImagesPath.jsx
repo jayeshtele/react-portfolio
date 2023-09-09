ed91460d41e252f7f6635bd66a613f9d6e1f7355
Jayesh Tele <110726388+jayeshtele@users.noreply.github.com>
2023-09-09 15:39:31 +0530

    Create ImagesPath.jsx

---
+++ src/ImagesPath.jsx
@@ -0,0 +1,152 @@
+import LaunchIcon from "@mui/icons-material/Launch";
+import GitHubIcon from "@mui/icons-material/GitHub";
+
+export const react = [
+  {
+    id: 0,
+    linkPath: "https://coindasher.netlify.app/",
+    gitHubPath: "",
+    imagePath: "/assets/coindasher.png",
+    linkIcon: <LaunchIcon />,
+    gitHubIcon: <GitHubIcon />,
+  },
+  {
+    id: 1,
+    linkPath: "",
+    gitHubPath: "",
+    imagePath: "/assets/melody.png",
+    linkIcon: <LaunchIcon />,
+    gitHubIcon: <GitHubIcon />,
+  },
+  {
+    id: 2,
+    linkPath: "",
+    gitHubPath: "",
+    imagePath: "/assets/TicTacToe.png",
+    linkIcon: <LaunchIcon />,
+    gitHubIcon: <GitHubIcon />,
+  },
+];
+
+export const javascript = [
+  {
+    id: 0,
+    linkPath: "",
+    gitHubPath: "",
+
+    imagePath: "/assets/CashManager.png",
+    linkIcon: <LaunchIcon />,
+    gitHubIcon: <GitHubIcon />,
+  },
+  {
+    id: 1,
+    linkPath: "",
+    gitHubPath: "",
+
+    imagePath: "/assets/TranslaterApp.png",
+    linkIcon: <LaunchIcon />,
+    gitHubIcon: <GitHubIcon />,
+  },
+  {
+    id: 2,
+    linkPath: "",
+    gitHubPath: "",
+
+    imagePath: "/assets/Stocks.png",
+    linkIcon: <LaunchIcon />,
+    gitHubIcon: <GitHubIcon />,
+  },
+  {
+    id: 3,
+    linkPath: "",
+    gitHubPath: "",
+
+    imagePath: "/assets/ToDoApp.png",
+    linkIcon: <LaunchIcon />,
+    gitHubIcon: <GitHubIcon />,
+  },
+  {
+    id: 4,
+    linkPath: "",
+    gitHubPath: "",
+
+    imagePath: "/assets/Calculator.png",
+    linkIcon: <LaunchIcon />,
+    gitHubIcon: <GitHubIcon />,
+  },
+];
+
+export const htmlCss = [
+  {
+    id: 0,
+    linkPath: "",
+    gitHubPath: "",
+    imagePath: "/assets/Wedding.png",
+    linkIcon: <LaunchIcon />,
+    gitHubIcon: <GitHubIcon />,
+  },
+  {
+    id: 1,
+    linkPath: "",
+    gitHubPath: "",
+    imagePath: "/assets/Garage.png",
+    linkIcon: <LaunchIcon />,
+    gitHubIcon: <GitHubIcon />,
+  },
+  {
+    id: 2,
+    linkPath: "",
+    gitHubPath: "",
+    imagePath: "/assets/Gym.png",
+    linkIcon: <LaunchIcon />,
+    gitHubIcon: <GitHubIcon />,
+  },
+  {
+    id: 3,
+    linkPath: "",
+    gitHubPath: "",
+    imagePath: "/assets/Bakery.png",
+    linkIcon: <LaunchIcon />,
+    gitHubIcon: <GitHubIcon />,
+  },
+  {
+    id: 4,
+    linkPath: "",
+    gitHubPath: "",
+    imagePath: "/assets/Education.png",
+    linkIcon: <LaunchIcon />,
+    gitHubIcon: <GitHubIcon />,
+  },
+  {
+    id: 5,
+    linkPath: "",
+    gitHubPath: "",
+    imagePath: "/assets/Bank.png",
+    linkIcon: <LaunchIcon />,
+    gitHubIcon: <GitHubIcon />,
+  },
+  {
+    id: 6,
+    linkPath: "",
+    gitHubPath: "",
+    imagePath: "/assets/Cafe.png",
+    linkIcon: <LaunchIcon />,
+    gitHubIcon: <GitHubIcon />,
+  },
+  {
+    id: 7,
+    linkPath: "",
+    gitHubPath: "",
+    imagePath: "/assets/Fashion.png",
+    linkIcon: <LaunchIcon />,
+    gitHubIcon: <GitHubIcon />,
+  },
+  {
+    id: 9,
+    linkPath: "",
+    gitHubPath: "",
+    imagePath: "/assets/Doctor.png",
+    linkIcon: <LaunchIcon />,
+    gitHubIcon: <GitHubIcon />,
+  },
+];
