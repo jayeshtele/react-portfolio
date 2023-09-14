c4b64060c28f1689cb36a46c8234aedd30f75338
Jayesh Tele <110726388+jayeshtele@users.noreply.github.com>
2023-09-14 22:25:27 +0530

    Update ImagesPath.jsx

--- src/ImagesPath.jsx
+++ src/ImagesPath.jsx
@@ -3,15 +3,6 @@ import GitHubIcon from "@mui/icons-material/GitHub";
 
 export const react = [
   {
-    id: 0,
-    linkPath: "https://coindasher.netlify.app/",
-    gitHubPath: "https://github.com/jayeshtele/crypto-app",
-    imagePath: "/assets/coindasher.png",
-    linkIcon: <LaunchIcon />,
-    gitHubIcon: <GitHubIcon />,
-  },
-  {
-    id: 1,
     linkPath: "https://spotify-2-0-melody-music-clone.netlify.app/",
     gitHubPath: "https://github.com/jayeshtele/spotify-2.0-clone",
     imagePath: "/assets/melody.png",
@@ -19,7 +10,20 @@ export const react = [
     gitHubIcon: <GitHubIcon />,
   },
   {
-    id: 2,
+    linkPath: "https://jayeshtele-weather-app.netlify.app/",
+    gitHubPath: "https://github.com/jayeshtele/weather-app",
+    imagePath: "/assets/Weather.png",
+    linkIcon: <LaunchIcon />,
+    gitHubIcon: <GitHubIcon />,
+  },
+  {
+    linkPath: "https://coindasher.netlify.app/",
+    gitHubPath: "https://github.com/jayeshtele/crypto-app",
+    imagePath: "/assets/coindasher.png",
+    linkIcon: <LaunchIcon />,
+    gitHubIcon: <GitHubIcon />,
+  },
+  {
     linkPath: "https://jayeshtele-airbnb-clone.netlify.app/",
     gitHubPath: "https://github.com/jayeshtele/airbnb-clone",
     imagePath: "/assets/airbnb.png",
@@ -27,7 +31,6 @@ export const react = [
     gitHubIcon: <GitHubIcon />,
   },
   {
-    id: 3,
     linkPath: "https://jayesh-tic-tac-game.netlify.app/",
     gitHubPath: "https://github.com/jayeshtele/Tic-Tac-Toe-App",
     imagePath: "/assets/TicTacToe.png",
@@ -38,7 +41,6 @@ export const react = [
 
 export const javascript = [
   {
-    id: 0,
     linkPath: "https://cash-return-app.netlify.app/",
     gitHubPath: "https://github.com/jayeshtele/Cash-Register-Manager-App",
     imagePath: "/assets/CashManager.png",
@@ -46,7 +48,6 @@ export const javascript = [
     gitHubIcon: <GitHubIcon />,
   },
   {
-    id: 1,
     linkPath: "https://jayeshtele-fun-translate-app.netlify.app/",
     gitHubPath: "https://github.com/jayeshtele/Fun-Translation-App",
     imagePath: "/assets/TranslaterApp.png",
@@ -54,7 +55,6 @@ export const javascript = [
     gitHubIcon: <GitHubIcon />,
   },
   {
-    id: 2,
     linkPath: "https://stocks-profit-loss-giver-app.netlify.app/",
     gitHubPath: "https://github.com/jayeshtele/Stocks-Profit-Loss-App",
     imagePath: "/assets/Stocks.png",
@@ -62,7 +62,6 @@ export const javascript = [
     gitHubIcon: <GitHubIcon />,
   },
   {
-    id: 3,
     linkPath: "https://jayeshtele-todo-list.netlify.app/",
     gitHubPath: "https://github.com/jayeshtele/To-Do-App",
     imagePath: "/assets/ToDoApp.png",
@@ -70,7 +69,6 @@ export const javascript = [
     gitHubIcon: <GitHubIcon />,
   },
   {
-    id: 4,
     linkPath: "https://jayeshtele-calculator.netlify.app/",
     gitHubPath: "https://github.com/jayeshtele/Calculator",
     imagePath: "/assets/Calculator.png",
@@ -81,7 +79,6 @@ export const javascript = [
 
 export const htmlCss = [
   {
-    id: 0,
     linkPath: "https://jayeshtele-wedding.netlify.app/",
     gitHubPath: "https://github.com/jayeshtele/Wedding",
     imagePath: "/assets/Wedding.png",
@@ -89,7 +86,6 @@ export const htmlCss = [
     gitHubIcon: <GitHubIcon />,
   },
   {
-    id: 1,
     linkPath: "https://jayeshtele-garage.netlify.app/",
     gitHubPath: "https://github.com/jayeshtele/Garage",
     imagePath: "/assets/Garage.png",
@@ -97,7 +93,6 @@ export const htmlCss = [
     gitHubIcon: <GitHubIcon />,
   },
   {
-    id: 2,
     linkPath: "https://jayeshtele-gym.netlify.app/",
     gitHubPath: "https://github.com/jayeshtele/Gym",
     imagePath: "/assets/Gym.png",
@@ -105,7 +100,6 @@ export const htmlCss = [
     gitHubIcon: <GitHubIcon />,
   },
   {
-    id: 3,
     linkPath: "https://jayeshtele-bakery.netlify.app/",
     gitHubPath: "https://github.com/jayeshtele/Bakery",
     imagePath: "/assets/Bakery.png",
@@ -113,7 +107,6 @@ export const htmlCss = [
     gitHubIcon: <GitHubIcon />,
   },
   {
-    id: 4,
     linkPath: "https://jayeshtele-education.netlify.app/",
     gitHubPath: "https://github.com/jayeshtele/Education",
     imagePath: "/assets/Education.png",
@@ -121,7 +114,6 @@ export const htmlCss = [
     gitHubIcon: <GitHubIcon />,
   },
   {
-    id: 5,
     linkPath: "https://jayeshtele-bank.netlify.app/",
     gitHubPath: "https://github.com/jayeshtele/Bank",
     imagePath: "/assets/Bank.png",
@@ -129,7 +121,6 @@ export const htmlCss = [
     gitHubIcon: <GitHubIcon />,
   },
   {
-    id: 6,
     linkPath: "https://jayeshtele-coffee-shop.netlify.app/",
     gitHubPath: "https://github.com/jayeshtele/Coffee-Shop",
     imagePath: "/assets/Cafe.png",
@@ -137,7 +128,6 @@ export const htmlCss = [
     gitHubIcon: <GitHubIcon />,
   },
   {
-    id: 7,
     linkPath: "https://jayeshtele-fashion.netlify.app/",
     gitHubPath: "https://github.com/jayeshtele/Fashion",
     imagePath: "/assets/Fashion.png",
@@ -145,7 +135,6 @@ export const htmlCss = [
     gitHubIcon: <GitHubIcon />,
   },
   {
-    id: 8,
     linkPath: "https://jayeshtele-salon.netlify.app/",
     gitHubPath: "https://github.com/jayeshtele/Salon",
     imagePath: "/assets/Salon.png",
@@ -153,7 +142,6 @@ export const htmlCss = [
     gitHubIcon: <GitHubIcon />,
   },
   {
-    id: 9,
     linkPath: "https://jayeshtele-doctor.netlify.app/",
     gitHubPath: "https://github.com/jayeshtele/Doctor",
     imagePath: "/assets/Doctor.png",
