e262209b56c8f4731b6b557d2863d9881462e5cb
Jayesh Tele <110726388+jayeshtele@users.noreply.github.com>
2026-05-10 18:31:37 +0530

    Update ImagesPath.jsx

--- src/ImagesPath.jsx
+++ src/ImagesPath.jsx
@@ -17,7 +17,7 @@ export const react = [
     gitHubIcon: <GitHubIcon />,
   },
   {
-    linkPath: "",
+    linkPath: "https://cab-booking-app-beta.vercel.app/book",
     gitHubPath: "https://github.com/jayeshtele/Cab-Booking-App",
     imagePath: "/assets/Cab-Booking-App.png",
     linkIcon: <LaunchIcon />,
